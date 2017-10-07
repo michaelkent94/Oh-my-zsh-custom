c0a2dca3692244223fae349d90cbf70834a6e120
Michael Kent <michaelkent94@me.com>
2017-10-07 10:18:39 -0500

    Initial commit

---
+++ custom.zsh
@@ -0,0 +1,11 @@
+### Powerline
+export PATH="/usr/local/bin:$PATH:$HOME/Library/Python/2.7/bin"
+source /Users/michael/Library/Python/2.7/lib/python/site-packages/powerline/bindings/zsh/powerline.zsh
+
+### Aliases
+alias ls="ls -G"
+alias gcc="gcc-6"
+alias emacs="TERM=xterm-256color emacs -nw" # I set TERM here so that emacs themes work better inside tmux
+
+### Environment
+export EDITOR=emacs
